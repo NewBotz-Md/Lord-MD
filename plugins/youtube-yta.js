4c4884cb43e3acf522d51df5645d4c91eb335813
AmmarrBN <104218064+AmmarrBN@users.noreply.github.com>
2022-04-30 13:47:23 +0700

    Update youtube-yta.js

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,39 +1,113 @@
-let axios = require('axios')
-const fetch = require('node-fetch')
-let limit = 1024354
-const { servers, yta } = require('../lib/y2mate')
+let limit = 80
+import fetch from 'node-fetch'
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  let tumbnel = await await (await fetch(thumb)).buffer()
-  conn.sendButtonLoc(m.chat, tumbnel, `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link Download:* 
-${await shortlink(dl_link)}
-`, `Jika Audio tidak di kirim
-silahkan klik link di atas untuk mendownloadnya
-`.trim(), 'ok', 'ok', m)
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+  const isY = /y(es)/gi.test(args[1])
+  const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+  const limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
+  let audio, source, res, link, lastError, isLimit
+  for (let i in _audio) {
+    try {
+      audio = _audio[i]
+      isLimit = limitedSize < audio.fileSize
+      if (isLimit) continue
+      link = await audio.download()
+      if (link) res = await fetch(link)
+      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+      if (isLimit) continue
+      if (res) source = await res.arrayBuffer()
+      if (source instanceof ArrayBuffer) break
+    } catch (e) {
+      audio = link = source = null
+      lastError = e
+    }
+  }
+  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download audio')
+  if (!isY && !isLimit) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+*${htki} YOUTUBE ${htka}*
+
+*${htjava} Title:* ${title}
+*${htjava} Type:* mp3
+*${htjava} Filesize:* ${audio.fileSizeH}
+
+*L O A D I N G. . .*
+`.trim(), m)
+  if (!isLimit) await conn.sendFile(m.chat, source, title + '.mp3', `
+*${htki} YOUTUBE ${htka}*
+
+*${htjava} Title:* ${title}
+*${htjava} Type:* mp3
+*${htjava} Filesize:* ${audio.fileSizeH}
+
+*L O A D I N G. . .*
 `.trim(), m, null, {
-  asDocument: chat.useDocument
-})
+    asDocument: chat.useDocument
+  })
 }
-handler.help = ['mp3','a'].map(v => 'yt' + v + ` <url>`)
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> <without message>`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 
-handler.fail = null
 handler.exp = 0
+handler.register = true
 handler.limit = true
 
-module.exports = handler
+export default handlerlet limit = 80
+import fetch from 'node-fetch'
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
+  const isY = /y(es)/gi.test(args[1])
+  const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+  const limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
+  let audio, source, res, link, lastError, isLimit
+  for (let i in _audio) {
+    try {
+      audio = _audio[i]
+      isLimit = limitedSize < audio.fileSize
+      if (isLimit) continue
+      link = await audio.download()
+      if (link) res = await fetch(link)
+      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+      if (isLimit) continue
+      if (res) source = await res.arrayBuffer()
+      if (source instanceof ArrayBuffer) break
+    } catch (e) {
+      audio = link = source = null
+      lastError = e
+    }
+  }
+  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download audio')
+  if (!isY && !isLimit) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+*${htki} YOUTUBE ${htka}*
 
-async function shortlink(url){
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''}
+*${htjava} Title:* ${title}
+*${htjava} Type:* mp3
+*${htjava} Filesize:* ${audio.fileSizeH}
+
+*L O A D I N G. . .*
+`.trim(), m)
+  if (!isLimit) await conn.sendFile(m.chat, source, title + '.mp3', `
+*${htki} YOUTUBE ${htka}*
+
+*${htjava} Title:* ${title}
+*${htjava} Type:* mp3
+*${htjava} Filesize:* ${audio.fileSizeH}
+
+*L O A D I N G. . .*
+`.trim(), m, null, {
+    asDocument: chat.useDocument
+  })
+}
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> <without message>`)
+handler.tags = ['downloader']
+handler.command = /^yt(a|mp3)$/i
+
+handler.exp = 0
+handler.register = true
+handler.limit = true
+
+export default handler
